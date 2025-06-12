7eb9251d46c0ba76355277c77766aafd8f9ba56c
Hadi <rittihadi@gmail.com>
2025-06-12 21:48:20 +0700

    Match title to opengraph meta title

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Sauter</title>
+    <title>Sauter {{ $found ? "- $post->author_username" : '' }}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
